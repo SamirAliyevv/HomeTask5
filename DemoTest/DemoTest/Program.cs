a55855e336d3f726f86fc3a17a484f9653a145de
SamirAliyevv <samirsa@code.edu.az>
2023-01-23 11:46:30 -0800

    master

---
+++ DemoTest/DemoTest/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace DemoTest
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //    Verilmis ededi 10vahid artiran metod
+            //   bal 0-100 arasi ozu qalsin  gonderilmis deyer menfidirse 0,100den boyukdurse 100.
+            // verillmis ededler siyahisinda cut ededler qalan yeni bir array
+            //   
+
+            //int eded = 20;
+            //onvahidartiran(ref eded);
+            //Console.WriteLine(eded);
+            //int bal = 130;
+            //CheckPoints(ref bal);
+            //    //Console.WriteLine(bal);
+            //  int[] neweded= {2,3,4,5,8,12,25,23,36};
+            //    MakEven(ref neweded);
+            //    for (int i = 0; i < neweded.Length; i++)
+            //    {
+            //        Console.WriteLine(neweded[i]);
+            //    }
+        //    int[] reqem = { 1, 2, 3, 4, };
+        //    int newnum = 5;
+        //    addnumber(ref reqem,newnum);
+        //    for (int i = 0; i < reqem.Length; i++)
+        //    {
+        //        Console.WriteLine(reqem[i]);
+        //    }
+
+        //}
+
+        //static void onvahidartiran( ref int numbers)
+        //{
+
+        //    numbers += 10;
+
+
+        //}
+        //static void  CheckPoints (ref int points)
+        //{
+        //    if (points>100)
+        //    {
+        //        points = 100;
+        //    }
+        //    else if (points<0)
+        //    {
+        //        points = 0;
+        //    }
+
+        //}
+
+        //static void MakEven (ref int [] eded)
+        //{
+        //    int count = 0;
+        //    for (int i = 0; i < eded.Length; i++)
+        //    {
+        //        if (eded[i] %2==0)
+        //        {
+        //            count++;
+        //        }
+        //    }
+        //    int[] neweded= new int[count];
+        //    int j = 0;
+
+        //    for (int i = 0; i < eded.Length; i++)
+        //    {
+        //        if (eded[i]%2==0)
+        //        {
+        //            eded[i] = neweded[j];
+        //            j++;
+        //        }
+
+        //    }
+        //    eded = neweded;
+
+        //} 
+
+        //static void addnumber(ref int [] arr,int newnum)
+        //{
+        //    int[] newarray= new int[arr.Length+1];
+        //    int j = 0;
+        //    for (int i = 0; i < arr.Length; i++)
+        //    {
+        //        newarray[j] = arr[i];
+
+        //    }
+        //    newarray[newarray.Length - 1] = newnum;
+        //    arr = newarray; 
+        ////}
+
+
+    }
+}
